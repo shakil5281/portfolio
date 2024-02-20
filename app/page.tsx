e49265d95fddc202a5f9f757f53591b2dfb50ffc
shakil <shakilhossen307@gmail.com>
2024-02-20 08:30:36 +0600

    sum update

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import style from './css/herosection.module.css'
 import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export default function Homepage() {
   return (
-    <div className=''>
-      <div className=''>
-        <div className='relative h-96 after:absolute after:bottom-0 after:w-full after:h-32 after:bg-white'>
-          <Image
-          className='w-full bg-cover object-cover absolute top-0 left-0 -z-10 max-h-96'
-          width={2400}
-          height={2100}
-          src='https://images.unsplash.com/photo-1696388790726-68161096266e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-          alt=''
-          />
+    <div className='bg-gray-100 dark:bg-black'>
+      <div className='grid grid-cols-2 min-h-[90vh]'>
+        <div className='h-full flex flex-col justify-center items-center'>
+         <div className='inline-block'>
+         <h1 className='text-5xl font-semibold'>Change your mind set</h1>
+         </div>
+          <div className='inline-block translate-y-10'>
+          <Button>Contact Us</Button>
+          </div>
         </div>
-        <div>
-          <h1 className={`lg:text-5xl font-semibold  text-3xl ${style.primary_text} text-center flex justify-center items-center h-96`}>Change your thinking knowledge</h1>
+        <div className='h-full flex justify-center items-center'>
+            <Image
+              width={1200}
+              height={900}
+              src='https://astroship.web3templates.com/_astro/hero.6fdd0dc6_Z2mbqjy.webp'
+              alt='hero image'
+            />
         </div>
       </div>
     </div>
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,23 +1,27 @@
+import DarkMode from '@/app/Mode'
 import Link from 'next/link'
 import React from 'react'
 
 export default function Header() {
   return (
-    <div className='fixed z-30 top-0 left-0 w-full py-6 px-4'>
+    <div className='top-0 left-0 w-full py-6 px-4 shadow-lg dark:shadow-gray-300'>
       <div className='max-w-7xl m-auto'>
         <div className='flex justify-between items-center'>
           <div>
-            <h1 className='text-3xl font-semibold text-white'>Portfolio.</h1>
+            <h1 className='text-3xl font-semibold'>Portfolio.</h1>
           </div>
           <div className='hidden md:flex'>
             <div>
               <ul className='flex'>
                 {
                   ['Feature', 'Project', 'About us', 'Contact'].map((item, index) => (
-                    <li key={index}><Link className='px-4 py-4 text-white hover:text-gray-100 duration-200 text-md font-normal
+                    <li key={index}><Link className='px-4 py-4 duration-200 text-md font-normal
                     ' href='/'>{item}</Link> </li>
                   ))
                 }
+                <li className='-mt-2'>
+                  <DarkMode />
+                </li>
               </ul>
             </div>
           </div>
